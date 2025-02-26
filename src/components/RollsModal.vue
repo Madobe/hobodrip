a145502ded132b770dd36cd8faf61fee65d3cd70
YuanArib <yuan.arib@gmail.com>
2025-02-26 16:43:05 +0700

    Added comments

--- src/components/RollsModal.vue
+++ src/components/RollsModal.vue
@@ -8,6 +8,7 @@
         </div>
         <div class="modal-body">
             <div class="row">
+              <!-- Loop through rolledItems and display each item -->
               <div v-for="(count, item) in rolledItems" :key="item" class="col-4 text-center">
                 <DollFigure :doll="item.toString()" :dollsToPaths="dollsToPaths" :dupe="count - 1"></DollFigure>
               </div>
@@ -26,34 +27,46 @@ import { computed } from 'vue'
 import { usePullsStore } from '@/stores/pulls'
 import DollFigure from "@/components/DollFigure.vue"
 
+// List of doll names that include hyphens
 const hyphenatedDollNames = [
     "Mosin-Nagant"
 ]
 
+// Import all doll images from the specified directory
 const files: Record<string, string> = import.meta.glob(
     "@/assets/images/dolls/*.png",
     { eager: true, import: "default" },
 )
+
+// Extract paths of the imported images
 const paths = Object.values(files)
+
+// Extract doll names from the image paths
 const dolls = paths.map((path: string) => {
     let doll = path.replace(/^.*[\\/](.+).png$/, "$1")
 
+    // Check if the doll name includes any hyphenated names
     for (let i = 0; i < hyphenatedDollNames.length; i++) {
         if (doll.includes(hyphenatedDollNames[i])) return hyphenatedDollNames[i]
     }
 
+    // Remove any suffix after a hyphen
     doll = doll.replace(/-.*/, "")
 
     return doll
 })
+
+// Create a mapping of doll names to their image paths
 const dollsToPaths = dolls.reduce((accumulator, doll, i) => {
     return Object.assign(accumulator, {
         [doll]: paths[i]
     })
 }, {})
 
+// Access the pulls store
 const pulls = usePullsStore()
 
+// Compute the rolled items from the pulls store
 const rolledItems = computed(() => {
   return pulls.pulls.reduce((acc: { [x: string]: any }, pull: { name: string | string[] }) => {
     if (Array.isArray(pull.name)) {
