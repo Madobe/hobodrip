5ba5e9af1b5547e0181de117b4a85a7f1f71bc54
Madobe Nanami <sin_emerald@hotmail.com>
2025-02-28 04:04:40 -0700

    Remove Prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,6 @@
 import pluginVue from "eslint-plugin-vue"
 import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript"
 import pluginVitest from "@vitest/eslint-plugin"
-import skipFormatting from "@vue/eslint-config-prettier/skip-formatting"
 import stylistic from "@stylistic/eslint-plugin"
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
@@ -10,32 +9,31 @@ import stylistic from "@stylistic/eslint-plugin"
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
-    {
-        name: "app/files-to-lint",
-        files: ["**/*.{ts,mts,tsx,vue}"],
-    },
+  {
+    name: "app/files-to-lint",
+    files: [ "**/*.{ts,mts,tsx,vue}" ],
+  },
 
-    {
-        name: "app/files-to-ignore",
-        ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
-    },
+  {
+    name: "app/files-to-ignore",
+    ignores: [ "**/dist/**", "**/dist-ssr/**", "**/coverage/**" ],
+  },
 
-    pluginVue.configs["flat/essential"],
-    vueTsConfigs.recommended,
+  pluginVue.configs[ "flat/essential" ],
+  vueTsConfigs.recommended,
 
-    {
-        ...pluginVitest.configs.recommended,
-        files: ["src/**/__tests__/*"],
-    },
-    skipFormatting,
+  {
+    ...pluginVitest.configs.recommended,
+    files: [ "src/**/__tests__/*" ],
+  },
 
-    {
-        plugins: {
-            "@stylistic": stylistic,
-        },
-        rules: {
-            "@stylistic/array-bracket-spacing": ["error", "always"],
-            "@stylistic/space-in-parens": ["error", "always"],
-        },
+  {
+    plugins: {
+      "@stylistic": stylistic,
     },
+    rules: {
+      "@stylistic/array-bracket-spacing": [ "error", "always" ],
+      "@stylistic/space-in-parens": [ "error", "always" ],
+    },
+  },
 )
